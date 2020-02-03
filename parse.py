f4c9db486cf1a2a3531484c826a9766e5e68504b
theo <theotheo@MacBook-Air-de-Theo-2.local>
2020-02-03 16:47:53 +0100

    parse with error handling

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 import re
 import datefinder
+import sys, traceback
 
 # "graphic" interface of program
 def front():
@@ -30,10 +31,23 @@ def front():
     #final KeyWord before parsing
     print("Your final keywords: ",keyWord)
     
-    #Enter the word which will parse the file
+    #Enter the word which will parse the file if user don't want use "CRM"
     print(deco)
-    global input_crm
-    input_crm = input("Enter the WORD which will parse the file: ")
+    global tmp_crm
+    global crm_or_not
+    i = 0
+    while i < 1:
+        crm_or_not = input("Do you want to use 'CRM' as delimiter ? Yes or Not: ")
+        if crm_or_not == "Yes" or crm_or_not == "yes":
+            tmp_crm = "CRM"
+            i += 1
+        elif crm_or_not == "not" or crm_or_not == "Not":
+            print(deco)
+            tmp_crm = input("Enter the WORD which will parse the file: ")
+            i += 1
+        else:
+            print("Warning incorrect answer !")
+            # i += 1
     
     #Enter file to parse
     global file 
@@ -44,40 +58,47 @@ def front():
 def parse():
     front()
     #open the file
-    crm = input_crm + "(.*)"
+    crm = tmp_crm + "(.*)"
     if file.endswith('.txt'):
-        with open(file) as myfile:
-            for line in myfile:
-                # Search what is after 'CRM' 
-                result = re.search(crm, line)
-                if any(ele in line for ele in keyWord):
-                    print(deco)
-                    #String of all after CRM
-                    strAftCrm = result.group(1)
-                    if any(ele in strAftCrm for ele in keyWord):
-                        #To take off if for exemple string = 00/00/0000 à 00H00 will become 00/00/0000 00:00
-                        t = strAftCrm.replace('à', '').replace('H', ':').replace('h', ':')
-            
-                        #search if there is date in the string t
-                        matches = datefinder.find_dates(t)
-                        
-                        #create list containing matching word between keyWord list and strAftCrm String
-                        matchKeyWord_list=[]
-                        for i in range(len(keyWord)):
-                            if keyWord[i] in strAftCrm:
-                                matchKeyWord_list.append(keyWord[i])
-                        
-                        #create list containing date from string t
-                        date_list=[]
-                        for date in matches:
-                            date_list.append(date)
-                        
-                        #print corresponding keyword and date
-                        for i in range(len(matchKeyWord_list)):
-                            print (matchKeyWord_list[i], date_list[i])
-                            
-                        print(deco)
-                        return
+        try:
+            with open(file) as myfile:
+                for line in myfile:
+                    try:
+                        # Search what is after 'CRM' 
+                        result = re.search(crm, line)
+                        if any(ele in line for ele in keyWord):
+                            print(deco)
+                            #String of all after CRM
+                            strAftCrm = result.group(1)
+                            if any(ele in strAftCrm for ele in keyWord):
+                                #To take off if for exemple string = 00/00/0000 à 00H00 will become 00/00/0000 00:00
+                                t = strAftCrm.replace('à', '').replace('H', ':').replace('h', ':')
+                    
+                                #search if there is date in the string t
+                                matches = datefinder.find_dates(t)
+                                
+                                #create list containing matching word between keyWord list and strAftCrm String
+                                matchKeyWord_list=[]
+                                for i in range(len(keyWord)):
+                                    if keyWord[i] in strAftCrm:
+                                        matchKeyWord_list.append(keyWord[i])
+                                
+                                #create list containing date from string t
+                                date_list=[]
+                                for date in matches:
+                                    date_list.append(date)
+                                
+                                #print corresponding keyword and date
+                                for i in range(len(matchKeyWord_list)):
+                                    print (matchKeyWord_list[i], date_list[i])
+                                    
+                                print(deco)
+                    except AttributeError:
+                        # print(deco)
+                        print("Error ! Please restart program")
+        except FileNotFoundError:
+            print(deco)
+            print("The file can't be find")
     else:
         print(deco)
         print("Impossible ! Wrong file type")
